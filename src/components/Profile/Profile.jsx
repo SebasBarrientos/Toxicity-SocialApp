3be0fc09f5745cf8086eda111db891ffb30ad34a
Agustin Bernabe Erimbaue Lamontanaro <agustibernabe@gmail.com>
2024-06-05 12:51:41 +0200

    se agrega logica de profile para pintar info de usuario

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
 
 const Profile = () => {
+  const { user } = useSelector((state) => state.auth);
   return (
-    <div>Profile</div>
-  )
-}
+    <>
+      <div>
+        <div>Profile</div>
+        <div>
+          <p>{user.name}</p>
+          <p>{user.dateOfBirth}</p>
+        </div>
+        {/* <div>{console.log(user.posts)}</div>no vincula post con user */}
+      </div>
+    </>
+  );
+};
 
-export default Profile
+export default Profile;
