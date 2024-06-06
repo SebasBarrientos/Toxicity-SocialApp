da322d34651c37f915029da3dc5fd8c2b2c16277
Agustin Bernabe Erimbaue Lamontanaro <agustibernabe@gmail.com>
2024-06-06 13:21:43 +0200

    se agrega comentarios, nombre del usuario que comenta

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -14,17 +14,40 @@ const Post = () => {
     <div className="container mx-auto p-4">
       {posts.map((post) => {
         return (
-          <div key={post._id} className="bg-white shadow-md rounded-lg overflow-hidden mb-4">
+          <div
+            key={post._id}
+            className="bg-white shadow-md rounded-lg overflow-hidden mb-4"
+          >
             {console.log(post)}
-            <Link to={"/postdetail/" + post._id} className="no-underline hover:underline">
+            <Link
+              to={"/postdetail/" + post._id}
+              className="no-underline hover:underline"
+            >
               <h3>{post.userId?.userName}</h3>
               <div className="h-[350px] flex justify-center items-center bg-gray-100 shadow-lg">
-                <img src={`https://back-end-red-social.onrender.com/` + post.imgpost} alt="" className="max-h-[350px]" />
+                <img
+                  src={
+                    `https://back-end-red-social.onrender.com/` + post.imgpost
+                  }
+                  alt=""
+                  className="max-h-[350px]"
+                />
               </div>
               <div className="p-4">
                 <p className="text-gray-700 text-base">{post.caption}</p>
               </div>
+
             </Link>
+              <div>
+                {post.commentsIds.map((comment) => {
+                  return (
+                    <div>
+                      <p>{comment.userId.name}</p>
+                      <p>{comment.bodyText}</p>
+                    </div>
+                  );
+                })}
+              </div>
           </div>
         );
       })}
@@ -32,4 +55,4 @@ const Post = () => {
   );
 };
 
-export default Post; 
+export default Post;
