b2b3ffd492e8778e28fb55e347e4b4d37cc9fd41
Agustin Bernabe Erimbaue Lamontanaro <agustibernabe@gmail.com>
2024-06-13 13:38:33 +0200

    se agrega guards , componente notfoundy su scss

---
+++ src/components/NotFound/NotFound.jsx
@@ -0,0 +1,18 @@
+// NotFound.js
+
+import React from 'react';
+import './NotFound.scss'; // Importa el archivo SCSS
+
+const NotFound = () => {
+  return (
+    <div className="not-found">
+      <div className="not-found-content">
+        <h3>404 Page not found</h3>
+        <p>Oops! Looks like you've wandered off the beaten path.</p>
+        <p className="funny-quote">But hey, at least the scenery is nice!</p>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
