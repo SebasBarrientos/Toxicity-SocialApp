ca36045585ab9b28066989c18063d843f037e16d
Sebastian <sebas33696@gmail.com>
2024-06-14 01:13:33 +0200

    agregado mas scss

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -10,17 +10,21 @@ const Search = () => {
 
   const { caption } = useParams();
   const dispatch = useDispatch();
-  
+
   useEffect(() => {
     dispatch(getPostByName(caption))
     dispatch(searchByUserName(caption))
   }, [caption]);
 
-  return <div>
-    <h2>Posts</h2>
-    <Post/>
-    <h2>Users</h2>
-    <SearchByUserName/>
+  return <div >
+    <div className="title-search">
+      <h2>Posts found</h2>
+    </div>
+    <Post />
+    <div className="title-search">
+      <h2>Users found</h2>
+    </div>
+    <SearchByUserName />
   </div>;
 };
 
